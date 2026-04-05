cf0642c8a95798385a3f2d772d4900facba803a8
HieuTranThanh <tranhieu.hcmut@gmail.com>
2026-04-05 14:51:01 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@
 
 // Cấu hình tập trung để dễ chỉnh khi đổi nguồn dữ liệu hoặc rule nghiệp vụ.
 const DEFAULT_CONFIG = {
-  SHEET_ID: "",
-  SHEET_TAPDIEM: "",
-  SHEET_TAPDIEM_GID: "",
-  SHEET_CUSTOMER: "",
-  SHEET_CUSTOMER_GID: "",
-  FEEDBACK_FORM_URL: "",
-  TRACKING_SCRIPT_URL: "",
+  SHEET_ID: "1AkswJCHRClc7wAoagpRqtrG9OATm5_6qsRK0gTHZqcY",
+  SHEET_TAPDIEM: "Danh sách tập điểm S2",
+  SHEET_TAPDIEM_GID: "1472299907",
+  SHEET_CUSTOMER: "Danh sách khách hàng",
+  SHEET_CUSTOMER_GID: "341989509",
+  FEEDBACK_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLScf5EuKEWzllRuqEbjtLIIU_n0Z5HU7wZhpRW6NDkVh3Z36gw/viewform",
+  TRACKING_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbypr-0OMKp6UopO_-cET65QxdwTpeoMV9q13nJMdt3PBR-GtTHkKBrfHsdqTJR24bko/exec",
   POINT_RADIUS_METERS: 220,
   MAX_SALE_DISTANCE_METERS: 1000,
   MAX_SUGGESTED_POINTS: 3
